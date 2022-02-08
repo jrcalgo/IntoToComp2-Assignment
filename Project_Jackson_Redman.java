dc23f4e77874035babc17605035903a8002a618c
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2022-02-08 05:18:42 -0500

    Update Project_Jackson_Redman.java
    Added code for demo class

---
+++ Project_Jackson_Redman.java
@@ -0,0 +1,52 @@
+import Policy;
+import java.util.Scanner;
+
+public class Project_Jackson_Redman
+{
+    public static void main(String[] args)
+    {
+        String provider, firstName, lastName, smokingStatus;
+        int policyNum, age;
+        double height, weight;
+        double bmi;
+
+        Scanner keyboard = new Scanner(System.in);
+        Policy policy = new Policy();
+
+        System.out.print("Please enter the Policy Number: ");
+        policyNum = Integer.parseInt(keyboard.nextLine());
+        policy.setPolicyNum(policyNum);
+
+        System.out.print("\nPlease enter the Provider Name: ");
+        provider = keyboard.nextLine();
+        policy.setProvider(provider);
+
+        System.out.print("\nPlease enter the Policyholder's First Name: ");
+        firstName = keyboard.nextLine();
+        System.out.print("\nPlease enter the Policyholder's Last Name: ");
+        lastName = keyboard.nextLine();
+        policy.setHolderName(firstName, lastName);
+
+        System.out.print("\nPlease enter the policyholder's Age: ");
+        age = Integer.parseInt(keyboard.nextLine());
+        policy.setHolderAge(age);
+
+        System.out.print("\nPlease enter the Policyholder's Smoking Status (smoker / non-smoker): ");
+        smokingStatus = keyboard.nextLine();
+        policy.setSmokingStatus(smokingStatus);
+
+        System.out.print("\nPlease enter the Policyholder's Height (in inches): ");
+        height = Double.parseDouble(keyboard.nextLine());
+        System.out.print("\nPlease enter the Policyholder's Weight (in pounds): ");
+        weight = Double.parseDouble(keyboard.nextLine());
+        policy.setAttributes(height, weight);
+
+        keyboard.close();
+
+        bmi = policy.getBMI();
+        policy.dataOutput(bmi, policy.getPolicyCost(bmi));
+
+        System.exit(0);
+
+    }
+}
